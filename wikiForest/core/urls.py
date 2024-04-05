096a40a84c96645a2591e2cd710a5c62b56f444e
ChaserHati <catalina.zuniga.y@gmail.com>
2024-04-05 19:10:33 -0300

    correccion en core/urls.py

--- wikiForest/core/urls.py
+++ wikiForest/core/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import inicio, iniciar_sesion
 
 urlpatterns = [
-    path('inicio', inicio, name='inicio'),
+    path('', inicio, name='inicio'), #entre las comillas debe quedar vacio para que sea el primero en ser accedido, decia inicio antes
     path('iniciar_sesion', iniciar_sesion, name='iniciar_sesion'),
 ]
