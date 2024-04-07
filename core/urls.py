37b329496949b8481c3a7d9b4396671f1a96be39
Sebastian Corvalan <114147547+sebcorva@users.noreply.github.com>
2024-04-06 22:58:51 -0300

    añadiendo registro usuario

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import inicio, iniciar_sesion, cuenta, modificar_usuario
+from .views import inicio, iniciar_sesion, cuenta, registro
 
 urlpatterns = [
     path('inicio', inicio, name='inicio'), 
     path('', iniciar_sesion, name='iniciar_sesion'), #entre las comillas debe quedar vacio para que sea el primero en ser accedido
     path('cuenta', cuenta, name='cuenta'),
+    path('registro', registro, name='registro'),
 ]
--- core/views.py
+++ core/views.py
@@ -9,6 +9,18 @@ def inicio(request):
 def iniciar_sesion(request):
     return render(request, 'iniciar_sesion.html')
 
+def registro(request):
+    datos = {
+        'form' : UserForm()
+    }
+    if request.method=='POST':
+        formulario =UserForm(request.POST)
+        if formulario.is_valid:
+            formulario.save()
+            datos['mensaje'] = "Usuario creado exitosamente"
+
+    return render(request, 'registro.html', datos)
+
 def cuenta(request):
     usuario = Tabla_Usuario.objects.all()
     datos = {
